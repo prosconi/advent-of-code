74bb3cddcff093ff72bf4d0f5a878d4cb1633e95
Peter Rosconi <peterrosconi@gmail.com>
2024-12-16 13:02:23 -0500

    ugg

--- 2024/day16/Day16Part2.fsx
+++ 2024/day16/Day16Part2.fsx
@@ -100,6 +100,7 @@ let directions =
     ]
 
 type D = { Pos: (int * int); Dir: Facing }
+type D2 = { Pos: (int * int); Dir: Facing; Path: ResizeArray<int * int> }
 
 let printMaze (scores: Dictionary<D,int>) =
     let scores = scores |> Seq.map (fun kvp -> kvp.Key.Pos, kvp.Value) |> dict
@@ -120,31 +121,68 @@ let printMaze (scores: Dictionary<D,int>) =
         Console.WriteLine()
     Threading.Thread.Sleep(10)
 
+let printMazeWithPath (path: seq<int*int>) =
+    let dict = path |> Seq.map (fun pos -> pos, 0) |> dict
+    Console.Clear()
+    for y = 0 to height - 1 do
+        for x = 0 to width - 1 do
+            if dict.ContainsKey(x, y)
+            then
+                Console.ForegroundColor <- ConsoleColor.Green 
+                Console.Write 'X'
+                Console.ResetColor()
+            else
+                match getPiece (x, y) with
+                | Wall -> Console.Write '#'
+                | Start -> Console.Write 'S'
+                | End -> Console.Write 'E'
+                | Empty -> Console.Write ' '
+        Console.WriteLine()
+    Threading.Thread.Sleep(10)
+
 let dijsktra() =
     let scores = Dictionary()
-    scores.Add({ Pos = startPos; Dir = East }, 0)
+    let initialPosition = { Pos = startPos; Dir = East }
+    scores.Add(initialPosition, 0)
 
     let queue = PriorityQueue<_,_>()
-    queue.Enqueue({ Pos = startPos; Dir = East }, 0)
+    queue.Enqueue({ Pos = startPos; Dir = East; Path = ResizeArray([startPos]) }, 0)
+
+    let results = ResizeArray()
 
     while queue.Count > 0 do
         let d = queue.Dequeue()
+        let sd = { Pos = d.Pos; Dir = d.Dir }
         for direction, fn in directions do
             let nextPos = fn d.Pos
             match getPiece nextPos with
-            | End -> failwithf "Done! %A" (scores[d] + 1)
+            | End -> 
+                //scores.Add({ Pos = nextPos; Dir = direction }, scores[sd] + 1)
+                results.Add(scores[sd] + 1, d.Path)
             | Empty -> 
                 let additionalCost = if direction = d.Dir then 1 else 1001
 
-                let newScore = scores[d] + additionalCost
+                let newScore = scores[sd] + additionalCost
                 let nextD = { Pos = nextPos; Dir = direction }
                 if not <| scores.ContainsKey nextD then
                     scores.Add(nextD, Int32.MaxValue)
-                if newScore < scores[nextD] then
+                if newScore <= scores[nextD] then
                     scores[nextD] <- newScore
-                    queue.Enqueue(nextD, newScore)
+                    let arr = ResizeArray(d.Path)
+                    arr.Add(nextPos)
+                    queue.Enqueue({ Pos = nextPos; Dir = direction; Path = arr }, newScore)
             | _ -> 
                 ()
-    scores
+    results
 
-dijsktra()
+let results = dijsktra()
+let min = results |> Seq.map (fun (a, b) -> a) |> Seq.min
+let allMins = results |> Seq.filter (fun (a, b) -> a = min) |> Seq.toArray
+let allPaths = 
+    allMins
+    |> Seq.collect (fun (a, b) -> b)
+    |> Seq.distinct
+    |> Seq.toArray
+
+printMazeWithPath allPaths
+printfn "Length: %d" (allPaths.Length + 1)
