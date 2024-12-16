a19920c0ed688490e8c8a693bdce37285dcc141c
Peter Rosconi <peterrosconi@gmail.com>
2024-12-16 08:25:29 -0500

    blerg

--- 2024/day16/Day16.fsx
+++ 2024/day16/Day16.fsx
@@ -86,18 +86,22 @@ let east (x, y) = (x + 1, y)
 let west (x, y) = (x - 1, y)
 
 let printMaze path = 
+    Console.Clear()
     for y = 0 to height - 1 do
         for x = 0 to width - 1 do
             if Set.contains (x, y) path
-            then Console.Write 'X'
+            then
+                Console.ForegroundColor <- ConsoleColor.Green 
+                Console.Write 'X'
+                Console.ResetColor()
             else
                 match getPiece (x, y) with
                 | Wall -> Console.Write '#'
                 | Start -> Console.Write 'S'
                 | End -> Console.Write 'E'
-                | Empty -> Console.Write '.'
+                | Empty -> Console.Write ' '
         Console.WriteLine()
-    System.Threading.Thread.Sleep 100
+    Threading.Thread.Sleep(100)
 
 type Facing =
     | North
@@ -105,15 +109,68 @@ type Facing =
     | East
     | West
 
+let directions = 
+    [
+        North, north
+        South, south
+        East, east
+        West, west
+    ]
+
+let findAllDeadDends() =
+    seq {        
+        for y = 0 to height - 1 do
+            for x = 0 to width - 1 do
+                let piece = getPiece (x, y)
+                if piece = Empty then
+                    let neighbors = 
+                        [
+                            north (x, y)
+                            south (x, y)
+                            east (x, y)
+                            west (x, y)
+                        ]
+                        |> Seq.map getPiece
+                        |> Seq.filter (fun p -> p <> Wall)
+                        |> Seq.length
+                    if neighbors = 1 then
+                        yield x, y
+    }
+    |> Seq.toArray
+
+let blockAllDeadDends() =
+    let mutable exit = false
+    while not <| exit do
+        let deadDends = findAllDeadDends()
+        if deadDends.Length = 0 then
+            exit <- true
+        else
+            for x, y in deadDends do
+                data.[y].[x] <- Wall
+
+let dijkstra(graph, source) =
+    let dist = System.Collections.Generic.Dictionary<_,_>()
+    let prev = System.Collections.Generic.Dictionary<_,_>()
+    let q = System.Collections.Generic.PriorityQueue<_,_>()
+    for v in graph do
+        dist[v] <- Int32.MaxValue
+        prev[v] <- None
+        q.Enqueue(v)
+    dist[source] <- 0
+
+    while q.Count > 0 do
+        let u = []
+        for neighbor in u do
+            let alt = dist[u] + graph[u][neighbor]
+            if alt < dist[v] then
+                dist[v] <- alt
+                prev[v] <- u
+    dist, prev
+
 let rec go path turnCount facing pos =
-    // printMaze path
-    let directions = 
-        [
-            North, north
-            South, south
-            East, east
-            West, west
-        ]
+    if Set.count path > 500
+    then printMaze path
+
     seq {
         for direction, d in directions do
             let turning = 
@@ -136,6 +193,8 @@ let rec go path turnCount facing pos =
                     printfn "Found end!"
                     turnCount, (Set.count path) + 1
     }
+
+blockAllDeadDends()
 
 go Set.empty 0 East startPos
 |> Seq.filter (fun (turns, _) -> turns > 0)
