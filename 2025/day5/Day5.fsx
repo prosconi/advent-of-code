5dcb9a89b9bf77014d225bda5a6ef2ac2da4e464
Peter Rosconi <peterrosconi@gmail.com>
2025-12-05 20:27:05 -0500

    d5, not done

---
+++ 2025/day5/Day5.fsx
@@ -0,0 +1,157 @@
+// --- Day 5: Cafeteria ---
+// As the forklifts break through the wall, the Elves are delighted to discover that there was a cafeteria on the other side after all.
+
+// You can hear a commotion coming from the kitchen. "At this rate, we won't have any time left to put the wreaths up in the dining hall!" Resolute in your quest, you investigate.
+
+// "If only we hadn't switched to the new inventory management system right before Christmas!" another Elf exclaims. You ask what's going on.
+
+// The Elves in the kitchen explain the situation: because of their complicated new inventory management system, they can't figure out which of their ingredients are fresh and which are spoiled. When you ask how it works, they give you a copy of their database (your puzzle input).
+
+// The database operates on ingredient IDs. It consists of a list of fresh ingredient ID ranges, a blank line, and a list of available ingredient IDs. For example:
+
+// 3-5
+// 10-14
+// 16-20
+// 12-18
+
+// 1
+// 5
+// 8
+// 11
+// 17
+// 32
+// The fresh ID ranges are inclusive: the range 3-5 means that ingredient IDs 3, 4, and 5 are all fresh. The ranges can also overlap; an ingredient ID is fresh if it is in any range.
+
+// The Elves are trying to determine which of the available ingredient IDs are fresh. In this example, this is done as follows:
+
+// Ingredient ID 1 is spoiled because it does not fall into any range.
+// Ingredient ID 5 is fresh because it falls into range 3-5.
+// Ingredient ID 8 is spoiled.
+// Ingredient ID 11 is fresh because it falls into range 10-14.
+// Ingredient ID 17 is fresh because it falls into range 16-20 as well as range 12-18.
+// Ingredient ID 32 is spoiled.
+// So, in this example, 3 of the available ingredient IDs are fresh.
+
+// Process the database file from the new inventory management system. How many of the available ingredient IDs are fresh?
+
+let exampleLines = 
+    [|
+        "3-5"
+        "10-14"
+        "16-20"
+        "12-18"
+        ""
+        "1"
+        "5"
+        "8"
+        "11"
+        "17"
+        "32"
+    |]
+
+// let lines = System.IO.File.ReadAllLines(System.IO.Path.Combine(__SOURCE_DIRECTORY__, "Day5.txt"))
+
+let lines = exampleLines
+
+let emptyIndex = lines |> Array.findIndex (fun line -> line = "")
+
+type Range = { Start: int64; End: int64 }
+
+let parseRange (line: string) =
+    let parts = line.Split('-')
+    { Start = int64 parts[0]; End = int64 parts[1] }
+
+let ranges = lines[..emptyIndex - 1] |> Array.map parseRange
+
+let ids = lines[emptyIndex + 1..]
+
+let isInRange ranges id =
+    ranges
+    |> Array.exists (fun range -> id >= range.Start && id <= range.End)
+
+let part1 =
+    ids
+    |> Array.map int64
+    |> Array.filter (isInRange ranges)
+    |> Array.length
+
+// --- Part Two ---
+// The Elves start bringing their spoiled inventory to the trash chute at the back of the kitchen.
+
+// So that they can stop bugging you when they get new inventory, the Elves would like to know all of the IDs that the fresh ingredient ID ranges consider to be fresh. An ingredient ID is still considered fresh if it is in any range.
+
+// Now, the second section of the database (the available ingredient IDs) is irrelevant. Here are the fresh ingredient ID ranges from the above example:
+
+// 3-5
+// 10-14
+// 16-20
+// 12-18
+// The ingredient IDs that these ranges consider to be fresh are 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, and 20. So, in this example, the fresh ingredient ID ranges consider a total of 14 ingredient IDs to be fresh.
+
+// Process the database file again. How many ingredient IDs are considered to be fresh according to the fresh ingredient ID ranges?
+
+// scenario 0
+//           [ .... ] r2
+// [ .... ]           r1
+
+// scenario 1
+// [ .... ]           r2
+//          [ .... ]  r1
+
+// scenario 2
+// [ .... ]           r2
+//      [ .... ]      r1
+
+// scenario 3
+//       [ .... ]     r2
+//  [ .... ]          r1
+
+// scenario 4
+// [ .... ]           r2
+//  [ .. ]            r1
+
+// scenario 5
+//   [   ]            r2
+//  [ ... ]           r1
+
+let ranges2 = ranges |> Array.sortBy (fun r -> r.End - r.Start)
+let mutable runningCount = 0L
+let processedRanges = System.Collections.Generic.List<Range>()
+for i = 0 to ranges2.Length - 1 do
+    let mutable r1 = ranges2[i]
+
+    // recalculate r1 based on overlaps with processedRanges
+    for r2 in processedRanges do
+        if i = 3 then
+            printf "Checking overlap between %A-%A and %A-%A" r1.Start r1.End r2.Start r2.End
+
+        if r1.Start >= r2.End then
+            printfn "... none"
+        elif r1.End <= r2.Start then
+            printfn "... none"
+        elif r1.Start >= r2.Start && r1.End <= r2.End then
+            // r1 is completely inside r2
+            printfn "... Discarding range %A-%A as it is inside %A-%A" r1.Start r1.End r2.Start r2.End
+            r1 <- { Start = 0L; End = 0L } // mark as discarded
+        elif r1.Start <= r2.Start && r1.End >= r2.End then
+            // r2 is completely inside r1
+            printfn "... Modifying range %A-%A to remove inside %A-%A" r1.Start r1.End r2.Start r2.End
+            let leftPart = { Start = r1.Start; End = r2.Start - 1L }
+            let rightPart = { Start = r2.End + 1L; End = r1.End }
+            processedRanges.Add leftPart
+            r1 <- rightPart
+        else
+            let overlapStart = (max r1.Start r2.End) + 1L
+            let overlapEnd = max r1.End r2.End
+            printfn "... Modifying range %A-%A with overlap %A-%A" r1.Start r1.End overlapStart overlapEnd
+            r1 <- { r1 with Start = overlapStart; End = overlapEnd }
+    
+    processedRanges.Add r1
+
+processedRanges
+|> Seq.map (fun x -> sprintf "%A-%A" x.Start x.End)
+|> Seq.toArray
+
+let part2 =
+    processedRanges
+    |> Seq.sumBy (fun r -> r.End - r.Start)
